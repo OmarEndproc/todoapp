2d03574bb6274c77b15db2727373a265eafe0e98
Omar Galvez <ogalvez.ca@gmail.com>
2017-09-13 02:06:53 +0000

    Added delete action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -18,6 +18,33 @@ class TodosController < ApplicationController
     @todo = Todo.find(params[:id])
   end
 
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+  
+  def update
+     @todo = Todo.find(params[:id])
+    
+    if @todo.update(todo_params)
+      flash[:notice] = "todo was updated succesfully"
+      redirect_to todo_path(@todo)
+    else
+      render "edit"
+    end
+    
+  end
+  
+  def index
+    @todos = Todo.all
+  end
+  
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    flash[:notice] = "Todo was deleted succesfully"
+    redirect_to todos_path
+  end
+  
   private
   
   def todo_params
